12d53a6ee2133c4a0b4e65b0252960fcffbb0e58
Scott Gledhill <scottgledhill@hotmail.co.uk>
2018-11-03 12:50:58 +0000

    reducer function created

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ import { createStore } from 'redux';
 
 const store = createStore
 
+function reducer() {
+  return 'State'
+}
+console.log(store.getState())
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-git@github.com:ScottGledhill/redux-practice.gitgit 
