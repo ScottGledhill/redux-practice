701e88e216431a482813942283c2c1768a052d34
Scott Gledhill <scottgledhill@hotmail.co.uk>
2018-11-03 12:56:28 +0000

    action changes state

--- src/index.js
+++ src/index.js
@@ -5,12 +5,28 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 
-const store = createStore
+const store = createStore(reducer)
 
-function reducer() {
+function reducer(state, action) {
+  if (action.type === 'changeState') {
+    return action.payload.newState;
+  }
+
   return 'State'
 }
 console.log(store.getState())
+
+const action = {
+  type: 'changeState',
+  payload: {
+    newState: 'New State'
+  }
+}
+
+store.dispatch(action)
+
+console.log(store.getState())
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
